46823a1860e05c9a6166976b997eb30b41555be4
IG <idg7722@gmail.com>
2020-09-03 13:28:44 +0300

    Fixed bug, changed learning rate factor decrease method's name to "adapt_optimizer" (as it should be)

--- LearningRateFactorDecreaser.py
+++ LearningRateFactorDecreaser.py
@@ -4,7 +4,7 @@ class LearningRateFactorDecreaser(object):
         self.factor = factor
         self.epochs_interval = epochs_interval
 
-    def adjust_learning_rate(self, optimizer, epoch):
+    def adjust_optimizer(self, optimizer, epoch):
         """Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
         if (epoch % self.epochs_interval) == 0:
             for param_group in optimizer.param_groups:
